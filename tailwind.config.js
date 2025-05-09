238be7869de51ba2b2da7573324e9c5505c77e57
sniffdev <sniff@example.com>
2025-05-09 18:35:24 +0900

    fix: force tailwind css via cjs postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
