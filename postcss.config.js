5a642e617b1a3d9271f2d74af1f959c1f5f0c6d6
sniffdev <sniff@example.com>
2025-05-09 18:51:35 +0900

    fix: actually switch to tailwind v4 postcss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-import tailwind from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
+import tailwind from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
 
 export default {
   plugins: [
     tailwind(),
     autoprefixer()
   ]
-}
+};
